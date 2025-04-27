c0e01a6dd773f2749afa55f532fb60b8d9d10c71
Allex Alves <31211283+allexalvesdev@users.noreply.github.com>
2025-04-27 10:12:33 -0300

    correcao de inicialização de objetos

--- src/pages/Agenda.tsx
+++ src/pages/Agenda.tsx
@@ -6,6 +6,11 @@ import { formatDate, formatTime } from '@/utils/dateUtils';
 import { Badge } from '@/components/ui/badge';
 import { Link } from 'react-router-dom';
 
+const initialState = {
+  projects: [],
+  tasks: [],
+};
+
 const Agenda: React.FC = () => {
   const { state } = useAppContext();
   const { tasks, projects } = state;
